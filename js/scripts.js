f42ec28b73671b8be96817b5e2cc5dda0e8d77a5
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-02-27 20:46:03 -0500

    Ist Commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+alert('Hello world');
+
+let age = 29;
+let name = 'Anne';
+let favFood = 'Poke';
+
+
+console.log(age)
+console.log(name)
+
+let myName = 'Juan';
+document.write(myName);
+myName = 'John Does';
+document.write(myName);
+document.write(favFood);
