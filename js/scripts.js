4a38a64acedc2ffe56fe011940bbacbf1bb37f09
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-02-28 15:27:48 -0500

    Introducing Pokemon Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,6 @@
-alert('Hello world');
-
-let age = 29;
-let name = 'Anne';
-let favFood = 'Poke';
-
-
-console.log(age)
-console.log(name)
-
-let myName = 'Juan';
-document.write(myName);
-myName = 'John Does';
-document.write(myName);
-document.write(favFood);
+let pokemonList = [
+  {name:'Pidgeot', height:1.5 , types: 'Flying'}
+  {name:'Arbok', height: 3.5, types: 'poison'}
+  {name:'Poliwhirl', height:1, types:'water'}
+  {name:'Sandshrew', height:0.6, types:'ground'}
+];
