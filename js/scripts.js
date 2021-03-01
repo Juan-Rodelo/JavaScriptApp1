7ef3a209fa24de8aa8b5cdd929c2add66ee05c4e
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-03-01 11:17:00 -0500

    Deleting Inline, Fixing Syntax in .js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-  {name:'Pidgeot', height:1.5 , types: 'Flying'}
-  {name:'Arbok', height: 3.5, types: 'poison'}
-  {name:'Poliwhirl', height:1, types:'water'}
+  {name:'Pidgeot', height:1.5 , types: 'Flying'},
+  {name:'Arbok', height: 3.5, types: 'poison'},
+  {name:'Poliwhirl', height:1, types:'water'},
   {name:'Sandshrew', height:0.6, types:'ground'}
 ];
