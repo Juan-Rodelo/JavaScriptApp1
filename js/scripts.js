2979a6e7e22e00f2ed23753158a4fd51963432f0
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-03-02 11:30:33 -0500

    Added for loop for array list. Also h2 HTML in Java

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ let pokemonList = [
   {name:'Poliwhirl', height:1, types:'water'},
   {name:'Sandshrew', height:0.6, types:'ground'}
 ];
+
+let string = '<h3>';
+let stringClose= '</h3>';
+//Not sure if it's the smartest way to do this but allowys to add them to the document.write
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 3) {
+    document.write(string + pokemonList[i].name + pokemonList[i].height + 'Woa that\'s big!' + stringClose);
+  }
+  else {
+  document.write(string + pokemonList[i].name + pokemonList[i].height + stringClose);
+  }
+}
