eadabea7c2fe1d71d4938f967aa46298b4f1d5b5
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-03-09 11:48:56 -0500

    Show Details Function

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,27 @@ else {
 function getAll(){
   return pokemonList;
 }
+
+function addListItem(poki){
+  let pList = document.querySelector(".pokemon-list");
+  let listPokemon = document.createElement("li"); // creates the li element
+  let button = document.createElement("button"); //Creates the Button element in the document
+  button.innerText=poki.name; //Puts the text inside the button
+  button.classList.add("button-class") //adds the css "button-class" to the button var we just made
+  listPokemon.appendChild(button); //We put the element we just made into the <li> "listPokemon"
+  pList.appendChild(listPokemon); // We put the Li into the class ".pokemon-list of the parent <ul>"
+  button.addEventListener("click", showDetails(poki));
+}
+
+function showDetails(poki){
+  console.log(poki.name);
+}
+
 return {
-    getAll: getAll,
-    add: add
+    getAll : getAll,
+    add : add,
+    addListItem : addListItem,
+
   };
 })();
 
@@ -35,9 +53,31 @@ let Venusaur = 'Venusaur';
 
 pokemonRepository.add(Venusaur);
 pokemonRepository.add(Pidgey);
+
+
 pokemonRepository.getAll().forEach(function(poki) {
-  document.write('<h3>' + ' ' + poki.name + ' ' + poki.height + ' ' + poki.types + '</h3>');
+  pokemonRepository.addListItem(poki);
+
 });
+
+
+// pokemonRepository.getAll().forEach(function(poki) {
+//   document.write('<h3>' + ' ' + poki.name + ' ' + poki.height + ' ' + poki.types + '</h3>');
+// });
+
+
+//This is the example done inside the main pokemonRepository function
+//make a pokemon botton with all the pokemons
+// pokemonRepository.getAll().forEach(function(poki) {
+//   let pList = document.querySelector(".pokemon-list");
+//   let listPokemon = document.createElement("li"); // creates the li element
+//   let button = document.createElement("button"); //Creates the Button element in the document
+//   button.innerText=poki.name; //Puts the text inside the button
+//   button.classList.add("button-class") //adds the css "button-class" to the button var we just made
+//   listPokemon.appendChild(button); //We put the element we just made into the <li> "listPokemon"
+//   pList.appendChild(listPokemon); // We put the Li into the class ".pokemon-list of the parent <ul>"
+// });
+
 
 
 // function getPoki (poki) {
@@ -45,3 +85,5 @@ pokemonRepository.getAll().forEach(function(poki) {
 // }
 // pokemonRepository.getAll().forEach(getPoki);
 // Another way to do the forEach function
+
+//https://pokedex.org/#/pokemon/3
