4e93960f6cae56dce022df1b6f1eaa981cbc8947
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-03-11 10:23:07 -0500

    Load List and Details

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 
 
 let pokemonRepository = (function(){
-  let pokemonList = [
-    {name:'Pidgeot', height:1.5 , types: 'Flying'},
-    {name:'Arbok', height: 3.5, types: 'poison'},
-    {name:'Poliwhirl', height:1, types:'water'},
-    {name:'Sandshrew', height:0.6, types:'ground'}
-  ];
+  let pokemonList = [];
+
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
 function add(pokemon){
-  if (typeof(pokemon)=== 'object'){
+  if (
+    typeof pokemon === 'object' &&
+    "detailsUrl" in pokemon
+  ){
   pokemonList.push(pokemon);
+} else {
+  document.write('not an object');
 }
-else {
-  document.write('not and object');
 }
-}
+
 function getAll(){
   return pokemonList;
 }
@@ -24,10 +25,10 @@ function addListItem(poki){
   let listPokemon = document.createElement("li"); // creates the li element
   let button = document.createElement("button"); //Creates the Button element in the document
   button.innerText=poki.name; //Puts the text inside the button
-  button.classList.add("button-class") //adds the css "button-class" to the button var we just made
+  button.classList.add("button-class") //adds the css "button-class" to the button var we just made. ClassList represents the class of and element.
   listPokemon.appendChild(button); //We put the element we just made into the <li> "listPokemon"
   pList.appendChild(listPokemon); // We put the Li into the class ".pokemon-list of the parent <ul>"
-  button.addEventListener("click", function (){
+  button.addEventListener("click", function (event){
     showDetails(poki);
   })
 }
@@ -36,10 +37,63 @@ function showDetails(poki){
   console.log(poki.name);
 }
 
+function loadList() {
+  //fetch the apiUrl variable and then we convert it into a promise. The result is ging to be a response
+  //we use .then to return a promise. This is the funtion for a promise
+  return fetch(apiUrl).then(function (response) {
+  //we convert the response into a json. So basiccally turning the url into JSON language to talk to Javascript
+      return response.json();
+  }).then(function (json) {
+    //the results comes from te URL page.
+    json.results.forEach(function (item) {
+      let pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      //this goes to the function in the top
+      add(pokemon);
+      console.log(pokemon);
+    });
+  }).catch(function (e) {
+    console.error(e);
+  })
+}
+
+function loadDetails(item) {
+  //details url is coming from the prevous function
+   let url = item.detailsUrl;
+   //You are asking to take the url and turn it into a json
+   return fetch(url).then(function (response) {
+     return response.json();
+   }).then(function (details) {
+     // Now we add the details to the item
+     // those details come from the URL file
+     item.imageUrl = details.sprites.front_default;
+     item.height = details.height;
+     item.types = details.types;
+   }).catch(function (e) {
+     console.error(e);
+   });
+ }
+
+ function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+}
+
+
+
 return {
     getAll : getAll,
     add : add,
     addListItem : addListItem,
+    loadList: loadList,
+    loadDetails : loadDetails,
+    showDetails : showDetails
+
+
+
 
   };
 })();
@@ -56,12 +110,13 @@ let Venusaur = 'Venusaur';
 pokemonRepository.add(Venusaur);
 pokemonRepository.add(Pidgey);
 
-
+//Now we put the forEach list into the loading function.
+pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(poki) {
   pokemonRepository.addListItem(poki);
 
+  });
 });
-
 
 // pokemonRepository.getAll().forEach(function(poki) {
 //   document.write('<h3>' + ' ' + poki.name + ' ' + poki.height + ' ' + poki.types + '</h3>');
