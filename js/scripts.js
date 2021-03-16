a6b4f3b9da512d7cb16a8d26a8020968703e9e01
Juan-Rodelo <79281519+Juan-Rodelo@users.noreply.github.com>
2021-03-16 14:44:52 -0500

    Fixed Modal

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,13 @@
 let pokemonRepository = (function(){
 
   //MODAL
-  //Steps : 1. Create a modal-contianer div that is invisible in css. Here you are going to put the modal.
-    //2. Define all that you want in the modal in a function to show the modal.
-    //3.Craete a modal div, Create a close button. Make sure you can also close the model with escape or clicking out side the scree
-    //4. Create an event listener event to execute the show modeal function when you click on a pokemon button.
-    //
+
   //select modal div with css clase to display none
   let modalContainer = document.querySelector('#modal-container');
   //create a function to display the modalContainer
   function showModal (pokemon){
     //we create a div to add our css class modal that difines the div
-    let modal = createElement("div");
+    let modal = document.createElement("div");
     modal.classList.add("modal");
     //Add the visible css display
     modalContainer.classList.add("is-visible");
@@ -22,28 +18,34 @@ let pokemonRepository = (function(){
 
 
     //Create a close botton that we will add later to our modal div
-    let closeBottonElement = document.createElement("button");
-        closeBottonElement.classList.add("modal-close");
-        closeBottonElement.classList.innerText = "Close";
+    let closeButtonElement = document.createElement("button");
+        closeButtonElement.classList.add("modal-close");
+        closeButtonElement.innerText = "Close";
         closeButtonElement.addEventListener('click', hideModal);
 
     let createname = document.createElement('h1');
         createname.classList.add('h1');
-        createname.innerHTML = cap(pokemon.name);
+        createname.innerHTML = pokemon.name;
 
-    let createheight = document.createElement('h2');
-        createheight.classList.add('h2');
+    let createimg = document.createElement('img');
+        createimg.classList.add('pokemon-img');
+        createimg.src = pokemon.imageUrl;
+        //.alt returns the alternate text of an image
+        createimg.alt = "Image of " + pokemon.name
+
+    let createheight = document.createElement('p');
+        createheight.classList.add('p');
         createheight.innerHTML = "Height: " + pokemon.height*10 + "cm";
 
-    let createweight = document.createElement('h2');
-        createweight.classList.add('h2');
+    let createweight = document.createElement('p');
+        createweight.classList.add('p');
         createweight.innerHTML = "Weight: " + pokemon.weight + "lbs";
 
-    let createtype = document.createElement('h2');
-        createtype.classList.add('h2');
+    let createtype = document.createElement('p');
+        createtype.classList.add('p');
         createtype.innerHTML = "Type: " + pokemon.types;
 
-    modal.appendChild(closeBottonElement);
+    modal.appendChild(closeButtonElement);
     modal.appendChild(createname);
     modal.appendChild(createheight);
     modal.appendChild(createweight);
@@ -78,14 +80,13 @@ let pokemonRepository = (function(){
   //Here we select the button we want to open with a click and show the model we created
   // document.querySelector('.pokemon-list').addEventListener('click', () => {
   //   showModal('name', 'height');
-  });
 //MODAL ENDS
 
 
 
-  let pokemonList = [];
+let pokemonList = [];
 
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 function add(pokemon){
   if (
@@ -102,6 +103,7 @@ function getAll(){
   return pokemonList;
 }
 
+//Create the button  for the list
 function addListItem(poki){
   let pList = document.querySelector(".pokemon-list");
   let listPokemon = document.createElement("li"); // creates the li element
@@ -133,7 +135,7 @@ function loadList() {
       };
       //this goes to the function above
       add(pokemon);
-      console.log(pokemon);
+      // console.log(pokemon);
     });
   }).catch(function (e) {
     console.error(e);
@@ -143,9 +145,10 @@ function loadList() {
 // use the detailsUrl property to load the detailed data for a given Pokémon. takes a Pokémon item as an argument:
 // This one gives image height and types. We want this to access details with a click.
 function loadDetails(item) {
-  //details url is coming from the prevous function
+  //detailsUrl is coming from the prevous function
    let url = item.detailsUrl;
-   //You are asking to take the url and turn it into a json
+   //You are asking to take the url as a promise with then this runs a function, and turn it into a json,
+   //you take this json and turn it also into a promise
    return fetch(url).then(function (response) {
      return response.json();
    }).then(function (details) {
@@ -154,19 +157,17 @@ function loadDetails(item) {
      item.imageUrl = details.sprites.front_default;
      item.height = details.height;
      item.types = details.types;
-     item.imageUrl = details.sprites.front_default;
-     item.types = details.types;
-    item.weight = details.weight;
+     item.weight = details.weight;
    }).catch(function (e) {
      console.error(e);
    });
  }
 
- // function is executed when a user clicks on a Pokémon
+ // function is executed in addListItem when a user clicks on a Pokémon
  function showDetails(item) {
   pokemonRepository.loadDetails(item).then(function () {
   showModal(item);
-    });
+  });
 }
 
 return {
@@ -181,48 +182,10 @@ return {
 
 //Now we put the forEach list into the loading function.
 //I forgot how this works
+// The load list as a promise gets put into the getall(the repository), forEach getAll we add the botton in the addListItem
 pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(poki) {
   pokemonRepository.addListItem(poki);
 
   });
 });
-
-// pokemonRepository.getAll().forEach(function(poki) {
-//   document.write('<h3>' + ' ' + poki.name + ' ' + poki.height + ' ' + poki.types + '</h3>');
-// });
-
-
-//This is the example done inside the main pokemonRepository function
-//make a pokemon botton with all the pokemons
-// pokemonRepository.getAll().forEach(function(poki) {
-//   let pList = document.querySelector(".pokemon-list");
-//   let listPokemon = document.createElement("li"); // creates the li element
-//   let button = document.createElement("button"); //Creates the Button element in the document
-//   button.innerText=poki.name; //Puts the text inside the button
-//   button.classList.add("button-class") //adds the css "button-class" to the button var we just made
-//   listPokemon.appendChild(button); //We put the element we just made into the <li> "listPokemon"
-//   pList.appendChild(listPokemon); // We put the Li into the class ".pokemon-list of the parent <ul>"
-// });
-
-
-
-// function getPoki (poki) {
-// document.write(poki.name + poki.height + poki.types);
-// }
-// pokemonRepository.getAll().forEach(getPoki);
-// Another way to do the forEach function
-
-//https://pokedex.org/#/pokemon/3
-
-// To pass a new poki I need to make it into an object to push it into the array
-// let Pidgey = {
-//   name:'Pidgey',
-//   height:0.3 ,
-//   types: 'Flying'
-// };
-//
-// let Venusaur = 'Venusaur';
-//
-// pokemonRepository.add(Venusaur);
-// pokemonRepository.add(Pidgey);
